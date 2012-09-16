9ed4cdf96dbcb2d373f8988d7223d8edfc75f309
Eric Mertens <emertens@gmail.com>
2012-09-15 18:24:51 -0700

    Translate colors and add line length test to junction editor

--- src/main/java/com/gmail/emertens/pdxtrackrouter/listeners/JunctionEditor.java
+++ src/main/java/com/gmail/emertens/pdxtrackrouter/listeners/JunctionEditor.java
@@ -14,6 +14,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
 import com.gmail.emertens.pdxtrackrouter.PdxTrackRouter;
 import com.gmail.emertens.pdxtrackrouter.events.PlayerUseCommandSignEvent;
 
@@ -23,7 +24,8 @@ final class JunctionEditException extends Exception {
 	public JunctionEditException(final String message) {
 		this.message = message;
 	}
-	public String getMessage() {
+
+	public String getErrorMessage() {
 		return message;
 	}
 }
@@ -31,7 +33,7 @@ final class JunctionEditException extends Exception {
 final class Cursor {
 	private final int index;
 	private final Block block;
-	
+
 	public Cursor(int index, Block block) {
 		this.index = index;
 		this.block = block;
@@ -44,17 +46,18 @@ final class Cursor {
 	public Block getBlock() {
 		return block;
 	}
-	
+
 }
 
 public final class JunctionEditor implements CommandExecutor, Listener {
 
+	private static final int MAX_SIGN_LENGTH = 15;
 	private final Map<Player,Block> selectedBlocks = new HashMap<Player,Block>();
-	
+
 
 	@Override
 	public boolean onCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
-		
+
 		// Obtain player
 		final Player player;
 		if (sender instanceof Player) {
@@ -63,19 +66,23 @@ public final class JunctionEditor implements CommandExecutor, Listener {
 			sender.sendMessage(ChatColor.RED + "This command is for players");
 			return true;
 		}
-		
+
 		// Obtain junction block
 		final Block selectedBlock = selectedBlocks.get(player);
 		if (selectedBlock == null || !checkJunctionBlock(selectedBlock)) {
 			player.sendMessage(ChatColor.RED + "No junction sign selected");
 			return true;
 		}
-	
+
 		try {
 			// Dispatch sub-command
 			if (args.length >= 2) {
 				final int index = Integer.parseInt(args[1]);
-				final String text = concatenateArgs(args, 2);
+				final String text = ChatColor.translateAlternateColorCodes('&', concatenateArgs(args, 2));
+
+				if (text.length() > MAX_SIGN_LENGTH) {
+					throw new JunctionEditException("Line too long");
+				}
 
 				if (args[0].equalsIgnoreCase("insert")) {
 					insertOperation(selectedBlock, index, text);
@@ -92,7 +99,7 @@ public final class JunctionEditor implements CommandExecutor, Listener {
 		} catch (NumberFormatException e) {
 			return false;
 		} catch (JunctionEditException e) {
-			player.sendMessage(ChatColor.RED + e.getMessage());
+			player.sendMessage(ChatColor.RED + e.getErrorMessage());
 			return true;
 		}
 	}
@@ -107,49 +114,49 @@ public final class JunctionEditor implements CommandExecutor, Listener {
 	}
 
 	private void deleteOperation(Player player, Block selectedBlock, int index) throws JunctionEditException {
-		
+
 		final Cursor cursor = normalizeIndex(selectedBlock, index);
 		selectedBlock = cursor.getBlock();
 		index = cursor.getIndex();
-		
+
 		Sign sign = null;
-				
+
 		while (true) {
 			final BlockState state = selectedBlock.getState();
-			
+
 			if (!(state instanceof Sign)) {
-				
+
 				if (sign != null) {
 					sign.setLine(3, "");
 					sign.update();
 				}
-				
+
 				return;
 			}
-			
+
 			final Sign newSign = (Sign) state;
-			
+
 			if (sign != null) {
 				sign.setLine(3, newSign.getLine(0));
 				sign.update();
 			}
-			
+
 			sign = newSign;
-			
+
 			for (int i = index; i < 3; i++) {
 				sign.setLine(i, sign.getLine(i + 1));
 			}
-			
+
 			index = 0;
 			selectedBlock = selectedBlock.getRelative(BlockFace.DOWN);
-		}	
+		}
 	}
 
 	private void insertOperation(Block block, int index, String text) throws JunctionEditException {
 		final Cursor cursor = normalizeIndex(block, index);
 		block = cursor.getBlock();
 		index = cursor.getIndex();
-		
+
 		while (true) {
 			final BlockState state = block.getState();
 			if (state instanceof Sign) {
@@ -160,13 +167,13 @@ public final class JunctionEditor implements CommandExecutor, Listener {
 					text = temp;
 				}
 				sign.update();
-				
+
 				index = 0;
 				block = block.getRelative(BlockFace.DOWN);
 			} else {
 				return;
 			}
-		}		
+		}
 	}
 
 	private Cursor normalizeIndex(final Block selectedBlock, int index)
@@ -175,13 +182,13 @@ public final class JunctionEditor implements CommandExecutor, Listener {
 		if (index < 1) {
 			throw new JunctionEditException("Index too large");
 		}
-		
+
 		Block cursor = selectedBlock;
 		while (true) {
 			if (!(cursor.getState() instanceof Sign)) {
 				throw new JunctionEditException("Index too large");
 			}
-			
+
 			if (index >= 4) {
 				cursor = cursor.getRelative(BlockFace.DOWN);
 				index -= 4;
@@ -202,7 +209,7 @@ public final class JunctionEditor implements CommandExecutor, Listener {
 		}
 		return builder.toString();
 	}
-	
+
 	@EventHandler(ignoreCancelled = true)
 	public void onPlayerUseCommandSign(PlayerUseCommandSignEvent event) {
 		if (PdxTrackRouter.isJunctionHeader(event.getSign().getLine(0))) {
@@ -212,7 +219,7 @@ public final class JunctionEditor implements CommandExecutor, Listener {
 			}
 		}
 	}
-	
+
 	private static boolean checkJunctionBlock(final Block block) {
 		final BlockState state = block.getState();
 		if (state instanceof Sign) {
