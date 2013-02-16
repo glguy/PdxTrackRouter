012497913f5665368e7f7bb43d5f1b86e495a392
Eric Mertens <emertens@gmail.com>
2013-02-16 15:51:53 -0800

    Check that the player's hand is empty for junction selection

--- src/main/java/com/gmail/emertens/pdxtrackrouter/listeners/JunctionEditor.java
+++ src/main/java/com/gmail/emertens/pdxtrackrouter/listeners/JunctionEditor.java
@@ -259,6 +259,10 @@ public final class JunctionEditor implements CommandExecutor, Listener {
 			return;
 		}
 
+		if (event.getItem() != null) {
+			return;
+		}
+
 		final Junction junction = Junction.makeJunction(block);
 		if (junction == null) {
 			return;
